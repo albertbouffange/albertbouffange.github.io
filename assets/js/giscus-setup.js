bd6886e6058ae4ab9e6263606b2ee8cc34808f0c
albertbouffange <albert.bouffange@gmail.com>
2025-11-14 15:45:57 +0000

    Deploying to gh-pages from @ albertbouffange/albertbouffange.github.io@162d96a8ce10f95d44090a7804b9253487ab23b3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "albertbouffange/albertbouffange",
+    "data-repo": "albertbouffange/albertbouffange.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
